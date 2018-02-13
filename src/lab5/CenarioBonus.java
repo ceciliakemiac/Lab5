1e37bc601c652db7ffe243e15425bdb428d9cb5e
ceciliakemiac <ceciliakemiac2@gmail.com>
2018-02-13 13:37:19 -0300

    documentação

--- src/lab5/CenarioBonus.java
+++ src/lab5/CenarioBonus.java
@@ -1,8 +1,21 @@
 package lab5;
 
+/**
+ * Representação de um cenário bonus. Cada cenário possui uma descrição, um número
+ * e o valor do bonus.
+ * 
+ * @author Cecília Kemiac
+ */
 public class CenarioBonus extends Cenario{
 	private int bonus;
 
+	/**
+	 * Constrói o cenário bonus através de seus atributos.
+	 * 
+	 * @param descricao a descrição do cenário.
+	 * @param numero o número que representa o cenário.
+	 * @param bonus o valor do bonus.
+	 */
 	public CenarioBonus(String descricao, int numero, int bonus) {
 		super(descricao, numero);
 		
@@ -12,15 +25,27 @@ public class CenarioBonus extends Cenario{
 		this.bonus = bonus;
 	}
 	
+	/**
+	 * Retorna o valor do bonus do cenário.
+	 */
 	@Override
 	public int getBonus() {
 		return this.bonus;
 	}
 	
+	/**
+	 * Retorna o valor do bonus do cenário em reais.
+	 * 
+	 * @return o bonus em reais.
+	 */
 	public double getBonusReais() {
 		return this.bonus / 100.00;
 	}
 	
+	/**
+	 * Retorna a representação em String do cenário bonus, no formato padrão de um cenário
+	 * adicionado o valor do bonus.
+	 */
 	@Override
 	public String toString() {
 		return super.toString() + String.format(" - R$ %.2f", getBonusReais());
